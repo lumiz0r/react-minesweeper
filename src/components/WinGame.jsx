06c381385b7f0d13d3079196bd8e718988c77a41
Christian Cuesta <christianluminor@gmail.com>
2023-07-26 10:13:31 +0200

    update

--- src/components/WinGame.jsx
+++ src/components/WinGame.jsx
@@ -1,14 +1,14 @@
 import SadEmoji from '../images/CryEmoji.gif';
 
 // eslint-disable-next-line react/prop-types
-function LoseGame({ resetGame }) {
+function WinGame({ resetGame }) {
   return (
     <div className="lose-game">
-      <h2>Game Over</h2>
+      <h2>You Win!</h2>
       <img src={SadEmoji} className="sad-emoji"/>
       <button className="reset-button" onClick={resetGame}>Try Again</button>
     </div>
   );
 }
 
-export default LoseGame;
+export default WinGame;
